608b27da770a25b21640e777ae78b9c80102511f
hannahrstannard <hannahrstannard@gmail.com>
2019-06-21 23:07:57 -0400

    Create toolbar.

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Hello, World!</div>
+  <md-toolbar class="md-large" md-elevation="5">
+    <h3 class="md-title">Default</h3>
+  </md-toolbar>
 </template>
 
 <script>
@@ -8,6 +10,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  .md-toolbar {
+    clip-path: polygon(
+      0 0,
+      100% 0,
+      100% 40%,
+      0 100%
+    );
+  }
 
+  .md-toolbar.md-large {
+    min-height: 256px;
+    align-content: inherit;
+}
 </style>
