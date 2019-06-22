2ef9bcde49468619757183ba34850f6637d10583
hannahrstannard <hannahrstannard@gmail.com>
2019-06-21 21:16:34 -0400

    Import VueMaterial so it can be used in the project.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
 Vue.config.productionTip = false
+//TODO: Import individual components.
+Vue.use(VueMaterial)
 
 new Vue({
   render: h => h(App),
