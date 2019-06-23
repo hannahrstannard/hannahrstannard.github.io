cd7edb981f94e95b6aa0dd73ef4742d64da84574
hannahrstannard <hannahrstannard@gmail.com>
2019-06-23 14:56:49 -0400

    Add dynamic sizing for the font-size based on the resolution.

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div />
+  <md-content class="home__md-content">
+    <p>
+      This is my website.
+    </p>
+  </md-content>
 </template>
 
 <script>
@@ -8,5 +12,11 @@ export default {
 </script>
 
 <style lang="scss">
-
+  .home__md-content {
+    background-color: black;
+    margin: 0 auto;
+    margin-left: 10vw;
+    margin-right: 10vw;
+    margin-top: 10vh;
+  }
 </style>
